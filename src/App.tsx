b9f2a1aec9fd067a12a8a1cf0111319d85326867
Ian Bondoc <ibondoc@linz.govt.nz>
2022-04-15 15:59:17 +1200

    display other identity attributes and created components for conditionally rendering based on login status

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { UserService } from "./services/UserService";
+import { UserService } from "./services";
+import { RenderForAuthenticated } from "./components/RenderForAuthenticated";
+import { RenderForAnonymous } from "./components/RenderForAnonymous";
 
 function App() {
   return (
     <div>
       <h1>
-        Hello {UserService.isLoggedIn() ? UserService.getUsername() : "guest"}
+        Hello {UserService.isAuthenticated() ? UserService.getName() : "guest"}
       </h1>
-      {UserService.isLoggedIn() ? (
+      <RenderForAuthenticated>
+        <ul>
+          <li>username: {UserService.getUsername()}</li>
+          <li>given name: {UserService.getGivenName()}</li>
+          <li>surname: {UserService.getSurname()}</li>
+          <li>email: {UserService.getEmail()}</li>
+        </ul>
         <button onClick={() => UserService.logout()}>Logout</button>
-      ) : (
+      </RenderForAuthenticated>
+      <RenderForAnonymous>
         <button onClick={() => UserService.login()}>Login</button>
-      )}
+      </RenderForAnonymous>
     </div>
   );
 }
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -6,8 +6,7 @@ const initKeycloak = (onAuthenticateCallback: () => void) => {
   keycloak
     .init({
       onLoad: "check-sso",
-      silentCheckSsoRedirectUri:
-        window.location.origin + "/silent-check-sso.html",
+      silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
       pkceMethod: "S256",
     })
     .then((authenticated) => {
@@ -21,16 +20,24 @@ const initKeycloak = (onAuthenticateCallback: () => void) => {
     .catch(console.error);
 };
 
-const isLoggedIn = () => keycloak.authenticated;
+const isAuthenticated = () => keycloak.authenticated;
 const login = () => keycloak.login();
 const logout = () => keycloak.logout();
 
 const getUsername = () => keycloak.tokenParsed?.preferred_username;
+const getName = () => keycloak.tokenParsed?.name;
+const getGivenName = () => keycloak.tokenParsed?.given_name;
+const getSurname = () => keycloak.tokenParsed?.family_name;
+const getEmail = () => keycloak.tokenParsed?.email;
 
 export const UserService = {
   initKeycloak,
-  isLoggedIn,
+  isAuthenticated,
   login,
   logout,
   getUsername,
+  getName,
+  getGivenName,
+  getSurname,
+  getEmail,
 };
